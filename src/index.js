6670ddf90b1b525845d3b0de0db9ffd944b47c15
Fellipe Brandao <fellipesilverio31@hotmail.com>
2022-01-17 21:05:44 -0300

    metodos http e tipo de parametros

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json())
+
 /**
  * métodos http
  * get - busca info
@@ -11,19 +13,33 @@ const app = express();
  * delete - deletar informação
  */
 
+/**
+ * Tipo de parâmetros
+ * 
+ * Route Params -> identificar um recurso para editar / deletar / buscar
+ * Query Params -> Paginação / filtro de busca
+ * Body Params -> Objetos para inserção / alteração
+ */
+
 app.get('/courses', (req, res) => {
+    const query = req.query;
+    console.log(query);
     return res.json(["curso 1", "curso 2", "curso 3"]);
 });
 
 app.post('/courses', (req, res) => {
+    const body = req.body;
+    console.log(body);
     return res.json(["curso 1", "curso 2", "curso 3", "curso 4"])
 })
 
 app.put('/courses/:id', (req, res) => {
+    const params = req.params;
+    console.log(params);
     return res.json(["curso 6", "curso 2", "curso 3", "curso 4"])
 })
 
-app.path('/courses/:id', (req, res) => {
+app.patch('/courses/:id', (req, res) => {
     return res.json(["curso 6", "curso 7", "curso 3", "curso 4"])
 })
 
